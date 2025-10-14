ae751daf47b1a77016d90e8a779dc8ce9f674e12
TAO <24b953029@stu.hit.edu.cn>
2025-10-14 21:18:37 +0800

    2025.10.14.1

--- envs/beat_block_hammer.py
+++ envs/beat_block_hammer.py
@@ -85,3 +85,10 @@ class beat_block_hammer(Base_Task):
         eps = np.array([0.02, 0.02])
         return np.all(abs(hammer_target_pose[:2] - block_pose[:2]) < eps) and self.check_actors_contact(
             self.hammer.get_name(), self.block.get_name())
+    
+    def compute_reward(self):
+        # 自定义奖励函数
+        if self.check_success():  # 如果任务成功
+            return 1.0  # 成功奖励
+        else:
+            return -0.01  # 每步的惩罚
