4f9450ec5a6e766933c4ab5a350504a1e2004b79
TAO <24b953029@stu.hit.edu.cn>
2025-12-30 21:48:07 +0800

    11

---
+++ collect_3d_data.bash
@@ -0,0 +1,8 @@
+
+bash collect_data.sh beat_block_hammer demo_3d_vision_easy 0
+bash collect_data.sh stack_blocks_two demo_3d_vision_easy 0
+bash collect_data.sh stack_blocks_three demo_3d_vision_easy 0
+
+bash collect_data.sh beat_block_hammer demo_3d_vision_hard 0
+bash collect_data.sh stack_blocks_two demo_3d_vision_hard 0
+bash collect_data.sh stack_blocks_three demo_3d_vision_hard 0
